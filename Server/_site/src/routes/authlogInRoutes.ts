e87c1d389e31399fe6723566c76b421ee39fadd5
Crypt1cMS <dylan.minaya12@gmail.com>
2024-10-27 22:11:08 -0400

    Removed gem - jekyll

--- Server/_site/src/routes/authlogInRoutes.ts
+++
@@ -1,9 +0,0 @@
-// routes/loginRoutes.ts
-import express from 'express';
-import * as loginController from '../controllers/authController';
-
-const router = express.Router();
-
-router.post('/login', loginController.login);
-
-export default router;
